6f2189fc9b4549a6fca0dc99c2f03d38e2e5750e
Hugo Anes <anesfh@gmail.com>
2025-07-03 18:10:45 +0200

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="top-0 left-0 fixed h-full border max-w-28 w-full break-words">
+      Navbar
+    </div>
+  );
+}
